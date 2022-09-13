0a4aea5277a8cd0bfbb5b8dbf323c59c4241ee4d
mao2z <ziac569@gmail.com>
2022-09-13 05:41:23 -0700

    Part on addendum

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Gebire Tseganeh
+ *
+ */
+
+#include "holberton.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
