0a89006b0ea03faf82e1330d077b0d67f5f9fbc4
mao2z <ziac569@gmail.com>
2022-10-31 07:34:08 -0700

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,47 @@
-#include "main.h"
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - reads a txt file and prints it to standard output .
- * @filename: name of the txt file to read .
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file to read
  * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print
+ * Return: actual numbers it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
-	char *buffer;
+	int fd, fd_read, fd_write;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
-		return (0);
-	buffer = malloc(letters);
-	if (buffer == NULL)
 	{
-		close(fd);
+		free(buff);
 		return (0);
 	}
-	r = read(fd, buffer, letters);
+	fd_read = read(fd, buff, letters);
+	if (fd_read == -1)
+		return (0);
+	fd_write = write(STDOUT_FILENO, buff, fd_read);
+	if (fd_write == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	if (fd_read != fd_write)
+		return (0);
+	free(buff);
 	close(fd);
-	if (r == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	w = write(STDOUT_FILENO, buffer, r);
-	free(buffer);
-	if (r != w)
-		return (0);
-	return (w);
+	return (fd_write);
 }
