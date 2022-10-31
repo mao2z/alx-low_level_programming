00df1eb2e514cb114b0cc43db81e0298ba5b8a6a
mao2z <ziac569@gmail.com>
2022-10-31 07:57:28 -0700

    Second fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,47 @@
 #include "main.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
-  * read_textfile - ...
-  * @filename: The source file
-  * @letters: Number of letters to reads and prints
-  *
-  * Return: ...
-  */
+ * read_textfile - fuction that read a text file and print it out the POSIX std
+ * out
+ * @filename: body of text to print.
+ * @letters: max char to print.
+ * Return: number of chars printed.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, readed;
-	char *buff = malloc(sizeof(char *) * letters);
+	int fd, err, rd;
+	char *buf;
 
-	if (!buff)
+	fd = err = rd = 0;
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
 
-	if (!filename)
+	buf = malloc(sizeof(char) * letters + 1);
+	if (!buf)
 		return (0);
-
-	fd = open(filename, O_RDONLY, 0600);
-	if (fd == -1)
+	rd = read(fd, buf, letters);
+	if (rd < 0)
+	{
+		free(buf);
 		return (0);
+	}
+	buf[letters] = '\0';
+	err = write(STDOUT_FILENO, buf, rd);
+	if (err <= 0)
+	{
+		free(buf);
+		return (0);
+	}
 
-	readed = read(fd, buff, letters);
-	write(STDOUT_FILENO, buff, readed);
-
-	free(buff);
+	free(buf);
 	close(fd);
-	return (readed);
+	return (rd);
 }
